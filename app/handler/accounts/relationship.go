8155c787c61a7397d568c290ed213b4d50a79bae
Ryo <gryo.1440@gmail.com>
2022-08-04 17:12:53 +0900

    add GET Relationships

---
+++ app/handler/accounts/relationship.go
@@ -0,0 +1,43 @@
+package accounts
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"yatter-backend-go/app/handler/auth"
+	"yatter-backend-go/app/handler/httperror"
+)
+
+// Request quey for `GET /v1/accounts/relationships``
+type RelationshipsQuery struct {
+	Username string
+}
+
+// Handle request for `GET /v1/accounts/relationships`
+func (h *handler) Relationships(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	follower := auth.AccountOf(r)
+
+	var q RelationshipsQuery
+	q.Username = r.URL.Query().Get("username")
+	a := h.app.Dao.Account()
+	followee, err := a.FindByUsername(ctx, q.Username)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	f := h.app.Dao.Follow() // domain/repository の取得
+	relation, err := f.FindRelation(ctx, *follower, *followee)
+	if err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(relation); err != nil {
+		httperror.InternalServerError(w, err)
+		return
+	}
+}
