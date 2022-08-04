f0cb1af6eebb7126a4ed2eea9b453a523bed10a6
Ryo <gryo.1440@gmail.com>
2022-08-04 17:54:54 +0900

    add unfollow

--- app/dao/follow.go
+++ app/dao/follow.go
@@ -28,7 +28,7 @@ func (r *follow) FindRelation(ctx context.Context, user object.Account, another 
 	relation := new(object.Relation)
 
 	entity := new(object.Follow)
-	err := r.db.QueryRowxContext(ctx, "select * from follow where follower_id = ? and followee_id = ?", user.ID, another.ID).StructScan(entity)
+	err := r.db.QueryRowxContext(ctx, "SELECT * FROM follow WHERE follower_id = ? AND followee_id = ?", user.ID, another.ID).StructScan(entity)
 	if err == nil {
 		relation.Following = true
 	} else if errors.Is(err, sql.ErrNoRows) {
@@ -37,7 +37,7 @@ func (r *follow) FindRelation(ctx context.Context, user object.Account, another 
 		return nil, fmt.Errorf("%w", err)
 	}
 
-	err = r.db.QueryRowxContext(ctx, "select * from follow where follower_id = ? and followee_id = ?", another.ID, user.ID).StructScan(entity)
+	err = r.db.QueryRowxContext(ctx, "SELECT * FROM follow WHERE follower_id = ? AND followee_id = ?", another.ID, user.ID).StructScan(entity)
 	if err == nil {
 		relation.FollowedBy = true
 	} else if errors.Is(err, sql.ErrNoRows) {
@@ -52,6 +52,16 @@ func (r *follow) FindRelation(ctx context.Context, user object.Account, another 
 // AddFollow : followerとfollowee間のフォロー関係を作成
 func (r *follow) AddFollow(ctx context.Context, follower object.Account, followee object.Account) error {
 	_, err := r.db.ExecContext(ctx, "INSERT INTO follow (follower_id, followee_id) VALUES (?, ?)", follower.ID, followee.ID)
+	if err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	return nil
+}
+
+// DeleteFollow : followerとfollowee間のフォロー関係を削除
+func (r *follow) DeleteFollow(ctx context.Context, unfollower object.Account, unfollowee object.Account) error {
+	_, err := r.db.ExecContext(ctx, "DELETE FROM follow WHERE follower_id = ? AND followee_id = ?", unfollower.ID, unfollowee.ID)
 	if err != nil {
 		return fmt.Errorf("%w", err)
 	}
--- app/domain/repository/follow.go
+++ app/domain/repository/follow.go
@@ -11,4 +11,6 @@ type Follow interface {
 	FindRelation(ctx context.Context, user object.Account, another object.Account) (*object.Relation, error)
 	// Create follow relationship
 	AddFollow(ctx context.Context, follower object.Account, followee object.Account) error
+	// Delete follow relationship
+	DeleteFollow(ctx context.Context, unfollower object.Account, unfollowee object.Account) error
 }
